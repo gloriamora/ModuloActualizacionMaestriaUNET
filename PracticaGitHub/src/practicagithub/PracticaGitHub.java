d2b043ecc96958764e1f18af68c461dfbf8e7b98
Rosmary Ordoñez <rosmayelor@gmail.com>
2012-11-04 10:31:33 -0300

    Añadiendo Clase java.io
    Clase de java que tiene por objetivo señalar que ha ocurrido algún tipo
    error de entrada o salida de datos.

--- PracticaGitHub/src/practicagithub/PracticaGitHub.java
+++ PracticaGitHub/src/practicagithub/PracticaGitHub.java
@@ -4,6 +4,8 @@
  */
 package practicagithub;
 
+import java.io.IOException;
+
 /**
  *
  * @author Administrador
@@ -13,7 +15,7 @@ public class PracticaGitHub {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
     }
 }
