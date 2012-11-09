5f10fcb0ae86c2f9f58c8213483f4e5093352b5b
Ysmary Garcia <ysmary@gmail.com>
2012-11-09 04:27:31 -0400

    creando clase de libro
    en construccion de la clase libro

--- PracticaGitHub/src/practicagithub/Libro.java
+++ PracticaGitHub/src/practicagithub/Libro.java
@@ -4,10 +4,45 @@
  */
 package practicagithub;
 
-/**
- *
- * @author Gloria
- */
-public class Libro {
+public class Libro  extends Obra {
+
+  private String editorial;
+  private int nPaginas;
+  private Obra Oautor;
     
+    public Libro(String edit, String titu, String autoR, int aO,int pag)
+    {
+      this.editorial=edit;
+      this.Oautor.setTitulo(titu);
+      this.Oautor.setAutor(autoR);
+      this.Oautor.setAnoEdicion(aO);
+      this.nPaginas=pag;
+  }
+  
+   public String getEditorial()
+  {
+   return editorial;
+  }
+   
+    public void setEditorial(String edito)
+  {
+   this.editorial=edito;
+  }
+  
+   public int getNPaginas()
+  {
+   return nPaginas;
+  }
+  
+    public void setNPaginas(int paginas)
+  {
+   this.nPaginas=paginas;
+  }
+
+
+  public void imprimir()
+  {
+ 
+  }
+   
 }
