07c060975c03ec2c0e8c6a277d2e1f5cd345d52d
Gloria Mora <gloriamora@gmail.com>
2012-11-08 22:29:23 -0430

    Commit de Prueba sincronizacion entre NetBeans y plugin Github

---
+++ PracticaGitHub/src/practicagithub/Artista.java
@@ -0,0 +1,14 @@
+/*
+ * Clase ARTISTA
+ * Grupo Gloria, Maiguel, Rosmary, Ysmary
+ * Maestria en Informatica
+ */
+package practicagithub;
+
+/**
+ *
+ * @author Gloria
+ */
+public class Artista {
+    
+}
