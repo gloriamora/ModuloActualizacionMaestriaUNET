281eb21940a92bcf6d695a89aad28903459136d6
Maiguel <garmai77@gmail.com>
2012-11-09 22:48:34 -0430

    main correccion

--- PracticaGitHub/src/practicagithub/PracticaGitHub.java
+++ PracticaGitHub/src/practicagithub/PracticaGitHub.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * Autores de la Practica de GitHub
- * Integrantes: García Edward  C.I. 13.350.705
+ * Integrantes: García Maiguel  C.I. 13.350.705
  *              García Ysmary  C.I. 14.942.513
  *              Mora Gloria    C.I. 10.164.768
  *              Ordoñez Rosmary C.I.15.028.532 
