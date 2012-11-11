1f0bb4b299f4d18a4ab42ee7049171f801674fac
Gloria Mora <gloriamora@gmail.com>
2012-11-10 22:54:35 -0430

    Mofificacion Clase Pelicula

--- PracticaGitHub/src/practicagithub/Pelicula.java
+++ PracticaGitHub/src/practicagithub/Pelicula.java
@@ -1,2 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicagithub;
+
+/**
+ *
+ * @author Gloria
+ */
+public class Pelicula extends Obra{
+    private String productora;
+    private Artista[] interpretes;
+    
+    public Pelicula()
+    {
+    }
+    
+public Pelicula (String titu, String Autor, int aEd, String Prod, Artista[] inter)
+{
+      this.setTitulo(titu);
+      this.setAutor(Autor);
+      this.setAnoEdicion(aEd);
+      this.productora=Prod;
+      this.interpretes=inter;
+}
+
+
+public Artista[] getInterpretes()
+{
+return interpretes;
+}     
+        
+public void setInterpretes(Artista[] inter1)
+{
+this.interpretes=inter1;
+}  
+
+ public String getProductora()
+{
+ return productora;
+}     
+        
+public void setProductora(String productor)
+{
+this.productora=productor;
+}     
+
+public void imprimir()
+{
+ System.out.println(" Titulo "+this.getTitulo());
+ System.out.println(" Autor "+this.getAutor());
+ System.out.println(" Año de Edicion "+this.getAnoEdicion());   
+ System.out.println(" Productora "+productora);
+ System.out.println(" Interpretes "+interpretes);
+}
+    
+}
+
 
 
