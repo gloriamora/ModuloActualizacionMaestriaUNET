e63c580e265ce4ebed9038dc44ac9be5b9d3e167
Gloria Mora <gloriamora@gmail.com>
2012-11-10 22:31:39 -0430

    Mofificacion Clase Pelicula

--- PracticaGitHub/src/practicagithub/Pelicula.java
+++ PracticaGitHub/src/practicagithub/Pelicula.java
@@ -8,6 +8,35 @@ package practicagithub;
  *
  * @author Gloria
  */
-public class Pelicula {
+public class Pelicula extends Obra{
+    private String productora;
+    private Artista[] interpretes;
+    
+    public Pelicula()
+    {
+    }
+    
+public Artista[] getInterpretes()
+{
+return interpretes;
+}     
+        
+public void setInterpretes(Artista[] inter1)
+{
+this.interpretes=inter1;
+}  
+
+ public String getProductora()
+{
+ return productora;
+}     
+        
+public void setProductora(String productor)
+{
+this.productora=productor;
+}     
+
+
     
 }
+
